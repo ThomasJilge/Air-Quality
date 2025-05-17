21077e2778dd2b9f4db857ae54c4965a23ed5398
ThomasJilge <thomasjilge@gmx.de>
2025-05-17 19:21:59 +0200

    function search() added

--- src/app/air-quality/air-quality.component.ts
+++ src/app/air-quality/air-quality.component.ts
@@ -1,11 +1,36 @@
 import { Component } from '@angular/core';
+import { AirQualityService } from '../air-quality.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-air-quality',
-  imports: [],
   templateUrl: './air-quality.component.html',
-  styleUrl: './air-quality.component.scss'
+  styleUrls: ['./air-quality.component.scss'],
+  imports: [FormsModule,
+    CommonModule
+  ]
 })
 export class AirQualityComponent {
+  // city: string = '';
+  // searchCity = '';
+  // airData: any;
 
+  // constructor(private airQualityService: AirQualityService) {}
+
+  // search() {
+  //   if (!this.searchCity.trim()) {
+  //     alert('Bitte eine Stadt eingeben!');
+  //     return;
+  //   }
+  //   this.airQualityService.getLatestByCity(this.searchCity.trim()).subscribe({
+  //     next: (data) => {
+  //       this.airData = data;
+  //     },
+  //     error: (err) => {
+  //       console.error(err);
+  //       alert('Fehler beim Laden der Daten.');
+  //     },
+  //   });
+  // }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AirQualityService } from '../air-quality.service';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [FormsModule,
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  city: string = '';
+  searchCity = '';
+  airData: any;
+
+  constructor(private airQualityService: AirQualityService) {}
+
+  search() {
+    if (!this.searchCity.trim()) {
+      alert('Bitte eine Stadt eingeben!');
+      return;
+    }
+    this.airQualityService.getLatestByCity(this.searchCity.trim()).subscribe({
+      next: (data) => {
+        this.airData = data;
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Fehler beim Laden der Daten.');
+      },
+    });
+  }
 
 }
