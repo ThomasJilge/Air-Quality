3f1329a6b1e39ff2e7738a62d2e5e363dfc943fd
ThomasJilge <thomasjilge@gmx.de>
2025-05-17 15:09:33 +0200

    air quality component added

---
+++ src/app/air-quality/air-quality.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-air-quality',
+  imports: [],
+  templateUrl: './air-quality.component.html',
+  styleUrl: './air-quality.component.scss'
+})
+export class AirQualityComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from './footer/footer.component';
+import { AirQualityComponent } from "./air-quality/air-quality.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, 
+  imports: [RouterOutlet,
     HeaderComponent,
     FooterComponent,
     RouterModule,
-    RouterOutlet],
+    RouterOutlet, 
+    AirQualityComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
